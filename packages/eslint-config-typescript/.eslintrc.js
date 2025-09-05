d2ed320df5976532e36039f9f87115a262e59ea1
Aravindhan Palanisamy <aravindhan.palanisamy@ni.corp.natinst.com>
2025-09-05 10:57:48 +0530

    update ts

--- packages/eslint-config-typescript/.eslintrc.js
+++ packages/eslint-config-typescript/.eslintrc.js
@@ -1,8 +1,5 @@
 module.exports = {
     extends: [
-        'eslint:recommended',
-        'plugin:@typescript-eslint/recommended',
-        'plugin:@stylistic/disable-legacy',
         '@ni/eslint-config-javascript'
     ],
     plugins: [
